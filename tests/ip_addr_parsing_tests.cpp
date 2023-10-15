188348fcf5852ccb6e953925766739207d776bd3
Kumismar <xkouma02@vutbr.cz>
2023-10-15 15:29:21 +0200

    test: Add argument processing tests
    +Wrote two aux functions, refactored ipaddr tests

--- tests/ip_addr_parsing_tests.cpp
+++ tests/ip_addr_parsing_tests.cpp
@@ -8,84 +8,75 @@ TEST(IpAddressParsing, ValidIpAddresses)
 {
     IpAddressParser parser;
     std::string ipAddr = "111.111.111.111/24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), SUCCESS);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), SUCCESS);
     ipAddr = "0.0.0.0/32";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), SUCCESS);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), SUCCESS);
     ipAddr = "1.2.3.4/1";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), SUCCESS);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), SUCCESS);
     ipAddr = "192.0.1.12/12";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), SUCCESS);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), SUCCESS);
     ipAddr = "1.3.5.7/9";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), SUCCESS);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), SUCCESS);
     ipAddr = "32.42.42.42/31";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), SUCCESS);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), SUCCESS);
     ipAddr = "255.0.0.0/28";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), SUCCESS);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), SUCCESS);
+    ipAddr = "0.0.0.0/22";
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), SUCCESS);
 }
 
 TEST(IpAddressParsing, BadSeparators)
 {
     IpAddressParser parser;
     std::string ipAddr = "111,111.111.111/24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "111.111.111/111/24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "111.111-111.111/24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "111=111.111/111/24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "111.111.111.111=24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "111.111.111.111.24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
 }
 
 TEST(IpAddressParsing, BytesNotDivided)
 {
     IpAddressParser parser;
     std::string ipAddr = "123234.123.123/22";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "111.111111.111/24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "0.00.0/24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "1010.111.123/24";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
 }
 
 TEST(IpAddressParsing, MaskNotDivided)
 {
     IpAddressParser parser;
     std::string ipAddr = "123.234.123.12322";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "255.255.123.2551";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "111.233.64.255";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);    
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);    
     ipAddr = "63.234.2.51";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
 }
 
 TEST(IpAddressParsing, InvalidMaskNumbers)
 {
     IpAddressParser parser;
     std::string ipAddr = "123.234.123.123/33";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "255.255.123.255/0";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "255.255.123.255/234";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
     ipAddr = "255.255.123.255/-800";
-    EXPECT_EQ(parser.isIpAddress(ipAddr), FAIL);
+    EXPECT_EQ(parser.parseIPAddress(ipAddr), FAIL);
 }
-
-
-// TEST(IpAddressParsing, BadSeparator)
-// {
-    
-// }
-
-// TEST(IpAddressParsing, BadSeparator)
-// {
-    
-// }
