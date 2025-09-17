c40a100621b0595180ad549e8ce5ae37dca030d9
Ondřej Koumar <xkouma02@vutbr.cz>
2025-09-17 20:52:56 +0200

    Remove ISA project from root dir (whoopsie, wrong merge)

--- tests/aux/test_utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-#include <cstdint>
-
-char** stringVecToCharArr(std::vector<std::string>& strings);
-void deleteCharArr(char** strings, uint8_t numOfStrings);
